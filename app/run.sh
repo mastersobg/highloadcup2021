b196413be8ff77ac0f695ad8b38e6864bdaf47c9
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-01-26 00:10:39 +0300

    Support of memory profiling

--- app/run.sh
+++ app/run.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker run -i --rm --memory=2g --cpus=4 -p 9000:9000 \
+docker run -i --rm --memory=22g --cpus=4 -p 9000:9000 \
    --privileged --security-opt seccomp:unconfined --cap-add=ALL --network host  \
    --name highloadcup-app -t highloadcup-app
--- app/server-runner.sh
+++ app/server-runner.sh
@@ -10,7 +10,12 @@ echo "first argument" $1
 
 case $1 in
   profile-cpu)
-    LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprofiler.so env CPUPROFILE=/app/highloadcup2021.prof CPUPROFILESIGNAL=12 ./highloadcup2021 || echo "Crashed"
+    LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprofiler.so env CPUPROFILE=/app/highloadcup2021.prof CPUPROFILESIGNAL=12 \
+      ./highloadcup2021 || echo "Crashed"
+    ;;
+  profile-memory)
+    LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libtcmalloc.so env HEAPPROFILE=/app/highloadcup2021.prof HEAPPROFILESIGNAL=12 \
+      HEAP_PROFILE_ALLOCATION_INTERVAL=107374182400 HEAP_PROFILE_INUSE_INTERVAL=107374182400 ./highloadcup2021 || echo "Crashed"
     ;;
   *)
     ./highloadcup2021 || echo "Crashed"
