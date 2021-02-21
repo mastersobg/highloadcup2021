8378a52f85e4622c5520d7926bbcec72e492c92b
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-02-21 09:17:34 +0300

    Introduce rapidjson.
    Close #2

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <vector>
 #include "log.h"
+#include <rapidjson/document.h>
 
 int main() {
 
@@ -11,6 +12,11 @@ int main() {
     });
 
     debugf("Started %d", 1);
+    rapidjson::Document d;
+    char s[] = "{\"a\":1}";
+    d.ParseInsitu(s);
+    auto val = d["a"].GetInt();
+    debugf("value: %d", val);
 
     return 0;
 }
