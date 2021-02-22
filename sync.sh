0e4cad1120b91c5959c0b2ff0a5de6c7b153f651
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-02-22 21:54:23 +0300

    Skip ignore .git

--- sync.sh
+++ sync.sh
@@ -2,6 +2,6 @@
 
 source ~/.zshrc
 
-rsync -arzv --delete --exclude=.git --exclude=.idea --exclude=CMakeCache.txt --exclude=cmake-build-debug \
+rsync -arzv --delete --exclude=.idea --exclude=CMakeCache.txt --exclude=cmake-build-debug \
  --exclude=highloadcup2021.cbp --exclude=CMakeFiles --exclude=Makefile --exclude=Testing \
  --exclude=build --exclude=cmake_install.cmake /Users/gorbachev/sources/github/highloadcup2021/ build3:~/hlc
