6991ea76d8797742534d7af3e24e7f9c0f10bfb2
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-02-22 21:59:57 +0300

    Revert docker run options
    Server dump stopped working without them.

--- app/run.sh
+++ app/run.sh
@@ -3,12 +3,9 @@
 set -e
 
 docker run -i --rm  --memory=2g --cpus=4 -p 9000:9000 \
+   --privileged --security-opt seccomp:unconfined --cap-add=ALL \
    --network host  \
    -e ADDRESS=localhost -e Port=8000 -e Schema=http \
    --name highloadcup-app -t highloadcup-app
 
-#docker run -i --rm  --memory=2g --cpus=4 -p 9000:9000 \
-#   --privileged --security-opt seccomp:unconfined --cap-add=ALL --network host  \
-#   -e ADDRESS=localhost -e Port=8000 -e Schema=http \
-#   --name highloadcup-app -t highloadcup-app
 
