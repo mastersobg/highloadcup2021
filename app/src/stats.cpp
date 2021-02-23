47abb8e7d671e8576947f03a7a645c64030221de
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-02-23 07:22:16 +0300

    Remove get system time

--- app/src/stats.cpp
+++ app/src/stats.cpp
@@ -4,24 +4,24 @@
 #include "app.h"
 
 void Stats::print() const noexcept {
-    auto currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::steady_clock::now().time_since_epoch()
-    ).count();
-    auto timeElapsed = currentTime - startTime_.load();
+//    auto currentTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+//            std::chrono::steady_clock::now().time_since_epoch()
+//    ).count();
+//    auto timeElapsed = currentTime - startTime_.load();
     int64_t rps{0};
-    if (timeElapsed > 0) {
-        rps = requestsCnt_.load() * 1000 / timeElapsed;
-    }
+//    if (timeElapsed > 0) {
+//        rps = requestsCnt_.load() * 1000 / timeElapsed;
+//    }
 
     infof("Requests count: %lld", requestsCnt_.load());
     infof("RPS: %lld", rps);
-    infof("Time elapsed: %lld", timeElapsed);
+//    infof("Time elapsed: %lld", timeElapsed);
 }
 
 Stats::Stats() {
-    startTime_ = std::chrono::duration_cast<std::chrono::milliseconds>(
-            std::chrono::steady_clock::now().time_since_epoch()
-    ).count();
+//    startTime_ = std::chrono::duration_cast<std::chrono::milliseconds>(
+//            std::chrono::steady_clock::now().time_since_epoch()
+//    ).count();
 }
 
 void statsPrintLoop() {
