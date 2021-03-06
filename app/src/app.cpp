015737790408466b450ddc2eb3988de88274cc64
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-03-06 08:21:40 +0300

    Revert "Do not dig deeper than 5"
    This reverts commit 96b59030c45da1219132b143d429f7b62415857d.

--- app/src/app.cpp
+++ app/src/app.cpp
@@ -258,9 +258,6 @@ ExpectedVoid App::processCashResponse(Request &r, HttpResponse<Wallet> &resp) no
 }
 
 ExpectedVoid App::scheduleDigRequest(int16_t x, int16_t y, int8_t depth) noexcept {
-    if (depth > 5) {
-        return NoErr;
-    }
     if (state_.hasAvailableLicense()) {
         auto licenseId = state_.reserveAvailableLicenseId();
         if (licenseId.hasError()) {
