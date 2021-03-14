ff61869ed9a54939cd801a2f8ad4782794ab423c
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-03-14 21:42:11 +0300

    kMaxRps = 1000

--- app/src/const.h
+++ app/src/const.h
@@ -15,7 +15,7 @@ constexpr size_t kJsonParseBufferSize = kJsonParseBufferCap * kJsonBufferTypeSiz
 constexpr size_t kMaxApiRequestsQueueSize = 10'000'000;
 
 constexpr size_t kApiThreadCount = 10;
-constexpr int64_t kMaxRPS = 100'000'000;
+constexpr int64_t kMaxRPS = 1'000;
 
 constexpr size_t kFieldMaxX = 3'500;
 constexpr size_t kFieldMaxY = 3'500;
