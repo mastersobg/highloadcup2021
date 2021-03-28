283a4c25bbe71be5586eb233aab99ea6cd592ded
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-03-29 01:56:33 +0300

    Add min request delay

--- app/src/const.h
+++ app/src/const.h
@@ -40,6 +40,6 @@ constexpr size_t kMaxLicensesCount = 10;
 
 constexpr size_t kExploreConcurrentRequestsCnt{2};
 
-constexpr int kCashSkipThreshold = 690'000'00;
+constexpr int kCashSkipThreshold = 690'000'000;
 
 #endif //HIGHLOADCUP2021_CONST_H
--- app/src/rate_limiter.cpp
+++ app/src/rate_limiter.cpp
@@ -4,6 +4,8 @@
 
 
 const std::chrono::seconds Second(1);
+const std::chrono::microseconds lastRequestDelay(5000);
+const std::chrono::microseconds minLastRequestDelaySleep(500);
 
 RateLimiter::RateLimiter(int32_t rps) : maxRps_{rps}, totalCost_{0} {}
 
@@ -33,7 +35,16 @@ void RateLimiter::acquire(int32_t cost) {
         }
     }
     now = std::chrono::steady_clock::now();
+    if (!requests_.empty()) {
+        const auto &last = requests_.back();
+        auto sleepTime = last.ts_ + lastRequestDelay - now;
+        if (sleepTime > minLastRequestDelaySleep) {
+//            infof("sleep for %d", sleepTime);
+            std::this_thread::sleep_for(sleepTime);
+        }
+    }
 
+    now = std::chrono::steady_clock::now();
     requests_.emplace_back(cost, now);
     totalCost_ += cost;
 }
