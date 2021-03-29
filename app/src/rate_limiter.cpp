fabb8d221dc54e03ea0749addf35e04c622c28c0
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-03-29 05:19:09 +0300

    lastRequestDelay=50000

--- app/src/rate_limiter.cpp
+++ app/src/rate_limiter.cpp
@@ -4,7 +4,7 @@
 
 
 const std::chrono::seconds Second(1);
-const std::chrono::microseconds lastRequestDelay(5000);
+const std::chrono::microseconds lastRequestDelay(50000);
 const std::chrono::microseconds minLastRequestDelaySleep(500);
 
 RateLimiter::RateLimiter(int32_t rps) : maxRps_{rps}, totalCost_{0} {}
