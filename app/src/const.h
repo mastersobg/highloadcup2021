3342f130e98547a47d7c7cf30b0e8c567e2ea028
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-03-29 19:18:03 +0300

    Cash after 4th floor

--- app/src/const.h
+++ app/src/const.h
@@ -40,6 +40,6 @@ constexpr size_t kMaxLicensesCount = 10;
 
 constexpr size_t kExploreConcurrentRequestsCnt{2};
 
-constexpr int minDepthToCash{4};
+constexpr int minDepthToCash{5};
 
 #endif //HIGHLOADCUP2021_CONST_H
