162835e315058783f227f7259e7723349a5bab62
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-03-31 01:19:22 +0300

    threads count 26

--- app/src/const.h
+++ app/src/const.h
@@ -14,7 +14,7 @@ constexpr size_t kJsonParseBufferSize = kJsonParseBufferCap * kJsonBufferTypeSiz
 
 constexpr size_t kMaxApiRequestsQueueSize = 10'000'000;
 
-constexpr size_t kApiThreadCount = 13;
+constexpr size_t kApiThreadCount = 26;
 constexpr int64_t kMaxRPS = 1'000;
 
 constexpr size_t kFieldMaxX = 3'500;
