e4413e2a692dd61c25abbc30b08d94e79ced2a6f
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-03-31 20:26:10 +0300

    One more explore area

--- app/src/const.h
+++ app/src/const.h
@@ -24,8 +24,9 @@ struct ExploreAreaShift {
     int16_t height;
     int16_t width;
 };
-constexpr std::array<ExploreAreaShift, 3> kExploreAreas = {
+constexpr std::array<ExploreAreaShift, 4> kExploreAreas = {
         {
+                {3500, 1},
                 {50, 1},
                 {5, 1},
                 {1, 1}
