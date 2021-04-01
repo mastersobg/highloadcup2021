7f9236bdfc7dc6dc862ecff748f156eeae149e6a
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-04-01 20:23:35 +0300

    Count avg explore costs

--- app/src/stats.cpp
+++ app/src/stats.cpp
@@ -33,6 +33,8 @@ void Stats::print() noexcept {
     infof("Duplicate set explored: %lld", duplicateSetExplored_.load());
     infof("Total process time: %lld expore time: %lld", totalProcessResponseTime_.load(),
           totalProcessExploreResponseTime_.load());
+    infof("Avg explore request cost: %f",
+          (double) exploreRequestTotalCost_.load() / (double) exploreRequestsCnt_.load());
     if (treasuriesCnt_.load() > 0) {
         infof("Avg explore request per treasure: %f",
               (double) exploreRequestsCnt_.load() / (double) treasuriesCnt_.load());
@@ -185,6 +187,17 @@ void Stats::printTreasuriesDiggedCount() noexcept {
     }
     infof("Digged treasuries count: %d", cnt);
 
+}
+
+const std::vector<int64_t> exploreCostThresholds = {0, 1058, 1036, 1132, 1538, 2065, 2571, 3120, 3634, 4123, 4723,
+                                                    5306, 6201, 7336, 8594, 11055, 15592, 24018, 40434, 71600,
+                                                    129316};
+
+int64_t Stats::calculateExploreCost(int64_t area) noexcept {
+    int64_t pos = 0;
+    for (pos = 0; (int64_t) 1LL << pos <= area; pos++) {
+    }
+    return exploreCostThresholds[static_cast<size_t>(pos)];
 }
 
 void recordInFlightRequests() {
