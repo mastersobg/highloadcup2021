336e9063564e4f1c9a6112fcf4f242d96003dca0
Ivan Gorbachev <gorbachev.ivan@gmail.com>
2021-04-03 19:52:59 +0300

    Brute force

--- go/explore-brute-force/main.go
+++ go/explore-brute-force/main.go
@@ -18,89 +18,52 @@ func main() {
 func bruteForce() error {
 	best := float64(math.MaxFloat64)
 	var bestArea [][]int
-	//for k := 1500; k > 1000; k-- {
-	for i := 51; i > 1; i-- {
-		for j := 6; j > 1; j-- {
-			if i <= j {
-				continue
+	candidates := [][]int{
+		{3500, 32},
+		{3500, 16},
+		{3500, 16},
+		{3500, 8},
+		{3500, 4},
+		{3500, 2},
+		{3500, 1},
+		{1750, 1},
+		{875, 1},
+		{438, 1},
+		{219, 1},
+		{110, 1},
+		{55, 1},
+		{28, 1},
+		{14, 1},
+		{7, 1},
+		{4, 1},
+		{2, 1},
+	}
+	for mask := (1 << len(candidates)) - 1; mask > 0; mask-- {
+		var areas [][]int
+		for j := 0; j < len(candidates); j++ {
+			if mask&(1<<j) != 0 {
+				areas = append(areas, candidates[j])
 			}
-			areas := [][]int{
-				{3500, 8},
-				{3500, 4},
-				//{3500, 2},
-				{3500, 1},
-				//{1750, 1},
-				{875, 1},
-				//{438, 1},
-				{219, 1},
-				//{110, 1},
-				{55, 1},
-				//{28, 1},
-				{14, 1},
-				{7, 1},
-				{4, 1},
-				{2, 1},
-				{1, 1},
-				//{256, 256},
-				//{256, 128}
-				//{256, 64},
-				//{256, 32},
-				//{100, 16},
-				//{256, 4},
-				//{512, 1},
-				//{256, 1},
-				//{64, 1},
-				//{4, 1},
-				//{2, 1},
-				//{1, 1},
-
-				//{1025, 10},
-				////{2049, 1},
-				//{1025, 1},
-				//{513, 1},
-				//{257, 1},
-				//{129, 1},
-				//{65, 1},
-				//{33, 1},
-				//{17, 1},
-				//{9, 1},
-				//{5, 1},
-				//{3, 1},
-				//{2, 1},
-				//{1, 1},
-				//{3500, 1},
-				////{350, 1},
-				////{35, 1},
-				//{3, 1},
-				//{1, 1},
-				////{500, 1000},
-				//{400, 100},
-				////{400, 10},
-				////{400, 1},
-				//{40, 1},
-				//{4, 1},
-				//{1, 1},
-			}
-			ret, err := run(areas)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			if ret < best {
-				best = ret
-				bestArea = make([][]int, len(areas))
-				for i, v := range areas {
-					bestArea[i] = make([]int, len(v))
-					for j := range v {
-						bestArea[i][j] = v[j]
-					}
+		}
+		areas = append(areas, []int{1, 1})
+		ret, err := run(areas)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		if ret < best {
+			best = ret
+			bestArea = make([][]int, len(areas))
+			for i, v := range areas {
+				bestArea[i] = make([]int, len(v))
+				for j := range v {
+					bestArea[i][j] = v[j]
 				}
 			}
-			fmt.Printf("best cost per treasure: %v\n", best)
-			fmt.Printf("best areas: %v\n", bestArea)
 		}
+		fmt.Printf("best cost per treasure: %v\n", best)
+		fmt.Printf("best areas: %v\n", bestArea)
 	}
-	//}
 	return nil
 }
 
